3ad713d50e37b98bdede1db80d8d0efe853d9ba8
Carlos Heuberger <carlos.heuberger@atos.net>
2017-03-15 09:47:35 +0000

    initial

---
+++ src/java/cfh/maze/Maze.java
@@ -0,0 +1,20 @@
+package cfh.maze;
+
+import java.util.Arrays;
+
+public class Maze {
+    
+    public static final boolean WALL = false;
+    public static final boolean PATH = true;
+    
+
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//    final int width;
+//    final int height;
+//    final Node entry;
+//    final Node exit;
+    
+    
+}
---
+++ src/java/cfh/maze/Node.java
@@ -0,0 +1,42 @@
+package cfh.maze;
+
+public class Node {
+    
+    final int x;
+    final int y;
+    
+    private Node[] neighbours = new Node[Direction.size()];
+    
+    
+    Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    void setNeighbour(Direction dir, Node node) {
+        neighbours[dir.ordinal()] = node;
+    }
+    
+    public Node getNeighbour(Direction dir) {
+        return neighbours[dir.ordinal()];
+    }
+    
+    @Override
+    public int hashCode() {
+        return 39 * x + 17 * y;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Node) {
+            Node other = (Node) obj;
+            return other.x == this.x && other.y == this.y;
+        }
+        return super.equals(obj);
+    }
+    
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+}
